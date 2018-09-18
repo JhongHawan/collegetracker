a7085e7e3d08860509b2bb233c8bfa342a639719
Brian Jhong <brianjhong@outlook.com>
2018-09-18 11:46:29 -0700

    Version 1.0.0

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import itemReducer from './itemReducer';
+
+export default combineReducers({
+    item: itemReducer,
+    // Here you can have as many reducers as you want
+    // Like authReducer, todoReducer, etc. 
+}); 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Brian:Tiger1998@ds147942.mlab.com:47942/mem_shopping'
+}
