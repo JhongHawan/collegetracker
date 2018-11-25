e6cae39c1d119290e633e9645e259d42a328487a
Brian Jhong <brianjhong@outlook.com>
2018-11-24 16:19:15 -0800

    5

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
 
 export default combineReducers({
-    item: itemReducer,
+    item: itemReducer
     // Here you can have as many reducers as you want
     // Like authReducer, todoReducer, etc. 
 }); 
